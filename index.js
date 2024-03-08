f3fbb702f13f76205a5fb725456ec4c4bbf417dc
Owolabi Oluwatimileyin <Timiiturna@gail.com>
2024-03-08 19:35:45 +0100

    committing updated files to the repository

--- index.js
+++ index.js
@@ -1,111 +1,55 @@
-const displayEL = document.getElementById("mastyles-display")
-const buttonEL = document.querySelector(".mastyles-button")
-let display = 0
-const operator = ''
-let result = ''
-const previousNumber = ''
-const currentNumber = ''
-let hasSolve = false
-let hasCalculate = false;
-buttonEL.addEventListener('click', function(){
-
-});
-  function btn0(){
-    display = 0;
-    displayEL.textContent += display;
-  };
-  function btn1(){
-    display = 1;
-    displayEL.textContent += display;
-  };
-  function btn2(){
-    display = 2;
-    displayEL.textContent += display;
-  };
-  function btn3(){
-     display = 3;
-    displayEL.textContent += display;
-  };
-  function btn4(){
-     display = 4;
-    displayEL.textContent += display;
-  };
-  function btn5(){
-     display = 5;
-    displayEL.textContent += display;
-  };
-  function btn6(){
-     display = 6;
-    displayEL.textContent += display;
-  };
-  function btn7(){
-    display = 7;
-    displayEL.textContent += display; 
-  };
-  function btn8(){
-    display = 8;
-    displayEL.textContent += display;
-  };
-  function btn9(){
-    display = 9;
-    displayEL.textContent += display; 
-  };
-  function addBtn(){
-    display = '+';
-    displayEL.textContent += display;
-  };
-  function subtractbtn(){
-    display = '-';
-    displayEL.textContent += display;
-  };
-  function multiplybtn(){
-    display = '*';
-    displayEL.textContent += display;
-  };
-  function divideBtn(){
-    display = '/';
-    displayEL.textContent += display;
-  };
-  function percentBtn(){
-    display = '%'
-    displayEL.textContent += display;
-  };
-  function plusMinusBtn(){
-    display = '+/-';
-    displayEL.textContent += display;
-  };
-  function pointBtn(){
-   display = '.';
-    displayEL.textContent += display;
-  };
+const displayEL = document.getElementById("mastyles-display");
+let display = 0;
+let operator = '';
+let result = '';
+let previousNumber = '';
+let currentNumber = '';
+let hasSolve = false;
+let firstArr = [];
+let secondArr = [];
+console.log(firstArr)
+console.log(secondArr)
+    function getNumber(num){
+      if (display === 0 || display === 1){
+        firstArr.push(num);
+        display = 1;
+        previousNumber = Number(firstArr.join(''));
+        displayEL.value = previousNumber
+      }else if(display === 2){
+        secondArr.push(num);
+        currentNumber = Number(secondArr.join(''));
+        displayEL.value = currentNumber
+      }
+    }
+    function getOperator(op){
+      display = 2;
+      operator = op;
+      displayEL.value += operator
+    }
   function equalBtn(){
-     if (hasSolve === true && hasCalculate === true){
      if (operator === '+'){
        result = previousNumber + currentNumber
+       displayEL.value = result;
      }else if (operator === '-'){
        result = previousNumber - currentNumber
+       displayEL.value = result;
      }else if (operator === '*'){
        result =  previousNumber * currentNumber
+       displayEL.value = result;
      }else if (operator === '/'){
        result =  previousNumber / currentNumber
+       displayEL.value = result;
      }
-     return result
-   }
-   if (previousNumber + currentNumber){
-     return previousNumber + currentNumber
-   }else if (previousNumber - currentNumber){
-     return previousNumber - currentNumber
-   }else if (previousNumber * currentNumber){
-     return previousNumber * currentNumber
-   }else if (previousNumber / currentNumber){
-     return previousNumber / currentNumber
-   }
-    displayEL.textContent = `${previousNumber} ${operator} ${currentNumber} = ${result}`
-    return result
-  };
+   } 
   function Acbtn(){
-     display = ' ';
-    displayEL.textContent = ' '
+    displayEL.value = ''
+    display = 0;
+    previousNumber = null
+    currentNumber = null
+    firstArr = []
+    secondArr = []
+    operator = ''
+    result = 0
   }
   
 
